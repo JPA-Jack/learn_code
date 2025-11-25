9b9c9b38e0233e16d2f22e2ef64ad43513858a1f
JPA_Jack <jack.parsons@jpasolutions.co.uk>
2025-11-25 21:27:04 +0000

    Python class update - added method return

--- python_class.py
+++ python_class.py
@@ -7,6 +7,19 @@ class Archer:
     # Add methods which are related to the class
     def take_damage(self, damage):
         self.health -= damage
+    
+    def health_kit_check(self, health_kit_count):
+        answer = ""
+        if self.health < 20:
+            if health_kit_count >= 1:
+                answer = "Health Kit available"
+            else:
+                answer = "Once this fight is over, you must go shopping"
+        else:
+            answer = "Yo, do some fighting first"
+        return answer
+        
+
 
 # Assign istances of the class
 legolas = Archer()
@@ -17,5 +30,6 @@ print(legolas.health)
 print(bard.arrows)
 
 # Apply the method, providing appropriate args
-legolas.take_damage(10)
+legolas.take_damage(20)
 print(legolas.health)
+print(legolas.health_kit_check(1))
