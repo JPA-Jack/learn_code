2c36a0a7ff922efc8a7566260b67655c1ba3cf93
JPA_Jack <jack.parsons@jpasolutions.co.uk>
2025-11-25 21:39:58 +0000

    Python class update - added constructors

--- python_class.py
+++ python_class.py
@@ -1,35 +1,13 @@
 # Initialise Class
 class Archer:
-    # Define Properties of the class in name/value pairs
-    health = 40
-    arrows = 10
+    # Define the constructor for the class
+    def __init__(self, name, armor, num_weapons):
+        self.name = name
+        self.armor = armor
+        self.num_weapons = num_weapons
 
-    # Add methods which are related to the class
-    def take_damage(self, damage):
-        self.health -= damage
-    
-    def health_kit_check(self, health_kit_count):
-        answer = ""
-        if self.health < 20:
-            if health_kit_count >= 1:
-                answer = "Health Kit available"
-            else:
-                answer = "Once this fight is over, you must go shopping"
-        else:
-            answer = "Yo, do some fighting first"
-        return answer
-        
+soldier_1 = Archer("Legolas", 10, 2)
 
-
-# Assign istances of the class
-legolas = Archer()
-bard = Archer()
-
-# Print Properties for each instance
-print(legolas.health)
-print(bard.arrows)
-
-# Apply the method, providing appropriate args
-legolas.take_damage(20)
-print(legolas.health)
-print(legolas.health_kit_check(1))
+print(soldier_1.name)
+print(soldier_1.armor)
+print(soldier_1.num_weapons)
