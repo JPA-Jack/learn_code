41585531917d70d91684c1398f917f5a6ad98cca
JPA_Jack <jack.parsons@jpasolutions.co.uk>
2025-11-25 21:09:36 +0000

    Python class intoduction

---
+++ python_class.py
@@ -0,0 +1,13 @@
+#Initialise Class
+class Archer:
+    #Define Attributes of the class in name/value pairs
+    health = 40
+    arrows = 10
+
+# Assign istances of the class
+legolas = Archer()
+bard = Archer()
+
+# Print Attributes for each instance
+print(legolas.health)
+print(bard.arrows)
