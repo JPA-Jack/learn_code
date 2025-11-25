acff0bb4fdc220896823452294bbca7c9c26a22a
JPA_Jack <jack.parsons@jpasolutions.co.uk>
2025-11-25 21:16:27 +0000

    Python class update - added methods

--- python_class.py
+++ python_class.py
@@ -1,13 +1,21 @@
-#Initialise Class
+# Initialise Class
 class Archer:
-    #Define Attributes of the class in name/value pairs
+    # Define Properties of the class in name/value pairs
     health = 40
     arrows = 10
+
+    # Add methods which are related to the class
+    def take_damage(self, damage):
+        self.health -= damage
 
 # Assign istances of the class
 legolas = Archer()
 bard = Archer()
 
-# Print Attributes for each instance
+# Print Properties for each instance
 print(legolas.health)
 print(bard.arrows)
+
+# Apply the method, providing appropriate args
+legolas.take_damage(10)
+print(legolas.health)
